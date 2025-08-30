2327d1c635d28f6b9f7053b18aeb9a8feccd3bfb
Arpan <arpansarkar.jsr5@gmail.com>
2025-08-31 02:55:14 +0530

    Added functionality for a user to edit video details

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -3,7 +3,7 @@ import VideoModel from "../models/videoModel.js";
 const uploadVideo = async (req, res) => {
     try {
         const { title, videoUrl, description, channel } = req.body;
-        const videoUploader = await req.user.id;
+        const videoUploader = req.user.id;
         const newVideo = new VideoModel({
             title,
             videoUrl,
@@ -18,4 +18,59 @@ const uploadVideo = async (req, res) => {
     }
 }
 
-export { uploadVideo}
+const editVideoDetails = async (req, res) => {
+    try {
+        const { title, thumbnailUrl, videoUrl, description } = req.body;
+        const videoId = req.params.videoId;
+        const userId = req.user.id;
+        let newTitle, newThumbnailUrl, newVideoUrl, newDescription;
+        // if (uploader !== userId) {
+        //     return res.status(403).json({ message: "You are not authorized to edit this video" });
+        // }
+        const videoToEdit = await VideoModel.findById(videoId);
+        if (!videoToEdit) {
+            return res.status(404).json({ message: "Video not found" });
+        }
+        if (videoToEdit.uploader.toString() !== userId) {
+            return res.status(403).json({ message: "You are not authorized to edit this video" });
+        }
+        // if (title.trim() === "" || videoUrl.trim() === "" || description.trim() === "") {
+        //     // return res.status(400).json({ message: "Please provide all the details" });
+        //     newTitle = videoToEdit.title;
+        //     newVideoUrl = videoToEdit.videoUrl;
+        //     newDescription = videoToEdit.description;
+        // }
+        if (title.trim() === "") {
+            newTitle = videoToEdit.title;
+        } else {
+            newTitle = title;
+        }
+        if (videoUrl.trim() === "") {
+            newVideoUrl = videoToEdit.videoUrl;
+        } else {
+            newVideoUrl = videoUrl;
+        }
+        if (description.trim() === "") {
+            newDescription = videoToEdit.description;
+        } else {
+            newDescription = description;
+        }
+        if (thumbnailUrl.trim() === "") {
+            newThumbnailUrl = videoToEdit.thumbnailUrl;
+        } else {
+            newThumbnailUrl = thumbnailUrl;
+        }
+        const newDetails = new VideoModel({
+            title: newTitle,
+            thumbnailUrl: newThumbnailUrl,
+            videoUrl: newVideoUrl,
+            description: newDescription,
+        });
+        const updatedVideo = await VideoModel.findByIdAndUpdate(videoId, newDetails);
+        return res.status(200).json({ message: "Video details updated successfully", video: updatedVideo });
+    } catch (err) {
+        return res.status(500).json({ message: "Internal server error - editVideoDetails", error: err.message });
+    }
+}
+
+export { uploadVideo, editVideoDetails }
--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -1,14 +1,18 @@
 import express from "express";
 import { verifyToken } from "../middlewares/verifyToken.js";
+import { uploadVideo, editVideoDetails } from "../controllers/videoController.js";
 
 const router = express.Router();
 
 // VIDEO ROUTES
 // Upload/Create Video
-router.post("/upload", verifyToken);
+router.post("/video/upload", verifyToken, uploadVideo);
 
 // Delete Video (only video owner can delete)
+
 // Update Video Details (only video owner can update)
+router.put("/video/update/:videoId", verifyToken, editVideoDetails);
+
 // Get Video and Details(comments, likes, dislikes, views)
 
 export default router;
