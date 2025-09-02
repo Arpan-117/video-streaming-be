bcd7042cf57ac659c19929ffe5ae9c85c2a36c06
Arpan <arpansarkar.jsr5@gmail.com>
2025-09-02 13:13:33 +0530

    Fixed the functionality for edit video details

--- controllers/videoController.js
+++ controllers/videoController.js
@@ -23,7 +23,7 @@ const editVideoDetails = async (req, res) => {
         const { title, thumbnailUrl, videoUrl, description } = req.body;
         const videoId = req.params.videoId;
         const userId = req.user.id;
-        let newTitle, newThumbnailUrl, newVideoUrl, newDescription;
+        // let newTitle, newThumbnailUrl, newVideoUrl, newDescription;
         // if (uploader !== userId) {
         //     return res.status(403).json({ message: "You are not authorized to edit this video" });
         // }
@@ -40,33 +40,46 @@ const editVideoDetails = async (req, res) => {
         //     newVideoUrl = videoToEdit.videoUrl;
         //     newDescription = videoToEdit.description;
         // }
-        if (title.trim() === "") {
-            newTitle = videoToEdit.title;
-        } else {
-            newTitle = title;
+        // if (title.trim() === "") {
+        //     newTitle = videoToEdit.title;
+        // } else {
+        //     newTitle = title;
+        // }
+        // if (videoUrl.trim() === "") {
+        //     newVideoUrl = videoToEdit.videoUrl;
+        // } else {
+        //     newVideoUrl = videoUrl;
+        // }
+        // if (description.trim() === "") {
+        //     newDescription = videoToEdit.description;
+        // } else {
+        //     newDescription = description;
+        // }
+        // if (thumbnailUrl.trim() === "") {
+        //     newThumbnailUrl = videoToEdit.thumbnailUrl;
+        // } else {
+        //     newThumbnailUrl = thumbnailUrl;
+        // }
+        // const newDetails = new VideoModel({
+        //     title: newTitle,
+        //     thumbnailUrl: newThumbnailUrl,
+        //     videoUrl: newVideoUrl,
+        //     description: newDescription,
+        // });
+        // const updatedVideo = await VideoModel.findByIdAndUpdate(videoId, newDetails);
+        if (title && title.trim() !== "") {
+            videoToEdit.title = title;
         }
-        if (videoUrl.trim() === "") {
-            newVideoUrl = videoToEdit.videoUrl;
-        } else {
-            newVideoUrl = videoUrl;
+        if (thumbnailUrl && thumbnailUrl.trim() !== "") {
+            videoToEdit.thumbnailUrl = thumbnailUrl;
         }
-        if (description.trim() === "") {
-            newDescription = videoToEdit.description;
-        } else {
-            newDescription = description;
+        if (videoUrl && videoUrl.trim() !== "") {
+            videoToEdit.videoUrl = videoUrl;
         }
-        if (thumbnailUrl.trim() === "") {
-            newThumbnailUrl = videoToEdit.thumbnailUrl;
-        } else {
-            newThumbnailUrl = thumbnailUrl;
+        if (description && description.trim() !== "") {
+            videoToEdit.description = description;
         }
-        const newDetails = new VideoModel({
-            title: newTitle,
-            thumbnailUrl: newThumbnailUrl,
-            videoUrl: newVideoUrl,
-            description: newDescription,
-        });
-        const updatedVideo = await VideoModel.findByIdAndUpdate(videoId, newDetails);
+        const updatedVideo = await videoToEdit.save();
         return res.status(200).json({ message: "Video details updated successfully", video: updatedVideo });
     } catch (err) {
         return res.status(500).json({ message: "Internal server error - editVideoDetails", error: err.message });
